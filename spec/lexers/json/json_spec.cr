87a3a5127da0b88e6d66d69ee81a1138f740adbf
hinata <syobon.hinata.public@gmail.com>
2018-12-20 18:55:55 +0900

    Fix spec of JSON

--- spec/lexers/json/json_spec.cr
+++ spec/lexers/json/json_spec.cr
@@ -3,7 +3,7 @@ require "../../spec_helper"
 require "../../../src/noir/lexers/json"
 
 describe Noir::Lexers::JSON do
-  it "can find canonical name 'css'" do
+  it "can find canonical name 'json'" do
     Noir.find_lexer("json").should be_a(Noir::Lexers::JSON)
   end
 
